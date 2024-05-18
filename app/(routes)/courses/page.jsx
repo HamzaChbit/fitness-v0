aee7a8bdad0dcb4d22d52cbf5b6d356a4d863477
hamza chbit <hamzachbit@gmail.com>
2024-05-18 13:21:53 +0000

    edit v2

--- app/(routes)/courses/page.jsx
+++ app/(routes)/courses/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageCourses = () => {
   return (
     <>
-    <div className='bg-background w-full mx-auto h-screen   ' >
+    <div className='bg-background w-full mx-auto h-full ' >
 <div className='flex justify-center items-center'data-aos="fade-down" >
 
 <div className='w-[80%] mx-auto my-5 py-8 bg-course-four flex flex-col justify-center items-center h-[80vh]   bg-no-repeat bg-cover  '> 
@@ -23,48 +23,48 @@ const PageCourses = () => {
     </div>
 
 
-    <div  className='bg-white mx-auto w-full h-[88vh] flex justify-center items-center' >
-        <div className='w-[60%] h-[66vh] ' >
+    <div  className='bg-white mx-auto w-full  flex justify-center items-center my-5' >
+        <div className='w-[60%] h-full ' >
             <div className='py-6 border-t-2 border-black flex row justify-between 'data-aos="fade-up">
                     <div className='flex flex-col gap-2' >
-                        <h1 className='font-medium text-xl' >MASTER TRAINER COURSE</h1>
+                        <h1 className='font-medium md:text-xl  text-md ' >MASTER TRAINER COURSE</h1>
                         <p>12 weeks, Monday-Wednesday 9am-3pm</p>
 
                     </div>          
                     <div   >
                         <h2 className='py-2' >$3,000</h2>
-                            <h1 className='font-bold text-xl' >Available spots</h1>
+                        <h1 className='font-bold md:text-xl text-md ' >Available spots</h1>
                     </div>
                     <div className='px-8 py-5 '>
-                    <button className='bg-body text-white px-4 py-2 hover:opacity-60 duration-200' >Book Now</button>
+                    <button className='bg-body text-white md:px-4  px-2 py-2 hover:opacity-60 duration-200 rounded-lg' >Book Now</button>
                     </div>
             </div>
             <div className='py-6 border-t-2 border-black flex row justify-between 'data-aos="fade-up">
                     <div className='flex flex-col gap-2' >
-                        <h1 className='font-medium text-xl' >MASTER TRAINER COURSE</h1>
+                    <h1 className='font-medium md:text-xl  text-md ' >MASTER TRAINER COURSE</h1>
                         <p>12 weeks, Monday-Wednesday 9am-3pm</p>
 
                     </div>          
                     <div   >
                         <h2 className='py-2' >$3,000</h2>
-                            <h1 className='font-bold text-xl' >Available spots</h1>
+                        <h1 className='font-bold md:text-xl text-md ' >Available spots</h1>
                     </div>
                     <div className='px-8 py-5 '>
-                    <button className='bg-body text-white px-4 py-2 hover:opacity-60 duration-200' >Book Now</button>
+                    <button className='bg-body text-white md:px-4  px-2  py-2 hover:opacity-60 duration-200 rounded-lg' >Book Now</button>
                     </div>
             </div>
             <div className='py-6 border-t-2 border-black flex row justify-between 'data-aos="fade-up">
                     <div className='flex flex-col gap-2' >
-                        <h1 className='font-medium text-xl' >MASTER TRAINER COURSE</h1>
+                    <h1 className='font-medium md:text-xl  text-md ' >MASTER TRAINER COURSE</h1>
                         <p>12 weeks, Monday-Wednesday 9am-3pm</p>
 
                     </div>          
                     <div   >
                         <h2 className='py-2' >$3,000</h2>
-                            <h1 className='font-bold text-xl' >Available spots</h1>
+                            <h1 className='font-bold md:text-xl text-md ' >Available spots</h1>
                     </div>
                     <div className='px-8 py-5 '>
-                    <button className='bg-body text-white px-4 py-2 hover:opacity-60 duration-200' >Book Now</button>
+                    <button className='bg-body text-white md:px-4  px-2 py-2 hover:opacity-60 duration-200 rounded-lg' >Book Now</button>
                     </div>
             </div>
         </div>
